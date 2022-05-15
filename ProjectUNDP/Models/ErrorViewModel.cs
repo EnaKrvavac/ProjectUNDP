d3ea5fc2573623509cab73f367ad4fa237c2d88d
EnaKrvavac <ena.krvavac@stu.ibu.edu.ba>
2022-05-15 19:49:08 +0200

    initial commit

---
+++ ProjectUNDP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectUNDP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
