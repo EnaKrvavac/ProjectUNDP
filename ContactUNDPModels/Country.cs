585c9aab356e42a885a63dbbdaa50739d149eb27
EnaKrvavac <ena.krvavac@stu.ibu.edu.ba>
2022-05-15 20:05:10 +0200

    Created the models project

---
+++ ContactUNDPModels/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactUNDPModels
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "Country Name is required")]
+        [StringLength(ContactManagerConstants.MAX_COUNTRY_NAME_LENGTH)]
+        public string CountryName { get; set; }
+    }
+}
